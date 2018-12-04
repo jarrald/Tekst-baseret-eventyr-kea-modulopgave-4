50b7c5fc1e057c9cf98a08fa219653dbb3e19f0a
Jarrald <novijgod@yahoo.dk>
2018-12-04 12:08:09 +0100

    Update StartMenu.java

--- code/StartMenu.java
+++ code/StartMenu.java
@@ -11,20 +11,69 @@ import java.awt.event.*;
 import java.awt.GridLayout;
 
 public class StartMenu {
+	
+	JFrame gameFrame;
+	Container con;
+	JPanel titlePanel, startButtonPanel, quitButtonPanel;
+	Font titleFont = new Font("Times New Roman", Font.PLAIN, 85);
+	Font buttonFont = new Font("Times New Roman", Font.PLAIN, 30);
+	Font textFont = new Font("Times New Roman", Font.PLAIN, 16);
+	JButton startButton, quitButton;
+	JTextArea mainTextArea;
+	TitleScreenHandler tsHandler = new TitleScreenHandler();
+
 	public static void main(String[] args)
 	{
-		JFrame gameFrame;
-		Container con;
-		JPanel titlePanel, startButton, quitButton;
+		//creating window/frame of program
+	    window = new JFrame();
+	    window.setSize(800,600);
+	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    window.getContentPane().setBackground(Color.black);
+	    window.setLayout(null);
+	    con = window.getContentPane();
+
+	    //creating a panel with a label on it (which will act as game title)
+	    titleNamePanel = new JPanel();
+	    titleNamePanel.setBounds(100, 100, 600, 150);
+	    titleNamePanel.setBackground(Color.black);
+	    titleNameLabel = new JLabel("PLACEHOLDER");
+	    titleNameLabel.setForeground(Color.white);
+	    titleNameLabel.setFont(titleFont);
+
+	    startButtonPanel = new JPanel();
+	    startButtonPanel.setBounds(300, 400, 200, 50);
+	    startButtonPanel.setBackground(Color.black);
+
+	    //creating start button and making it clickable
+	    startButton = new JButton("START");
+	    startButton.setBackground(Color.black);
+	    startButton.setOpaque(true);
+	    startButton.setBorderPainted(false);
+	    startButton.setForeground(Color.white);
+	    startButton.setFont(buttonFont);
+	    startButton.addActionListener(tsHandler);
+
+	    titleNamePanel.add(titleNameLabel);
+	    con.add(titleNamePanel);
+	    con.add(startButtonPanel);
+	    startButtonPanel.add(startButton);
+
+	    window.setVisible(true);
 	}
 	public ChoiceHandler _;
 
-	public void startGame() {
-
-		throw new UnsupportedOperationException();
+	public static void startGame() {
+		
 	}
 
 	private Player createPlayer(String name) {
 		throw new UnsupportedOperationException();
+	}
+	public class TitleScreenHandler implements ActionListener
+  	{
+	    public void actionPerformed(ActionEvent event)
+	    {
+	      startGame();
+	    }
 	}
 }
